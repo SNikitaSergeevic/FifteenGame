aabd6c67f70e71346b8c49e10b51fe94bb804fa2
SNikitaSergeevich <nikitoskasys@gmail.com>
2021-06-26 19:18:56 +0300

    Initial Commit

---
+++ Fifteen/FifteenApp.swift
@@ -0,0 +1,17 @@
+//
+//  FifteenApp.swift
+//  Fifteen
+//
+//  Created by ProstoKakTo on 26.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct FifteenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
